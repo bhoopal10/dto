182d5d41e99c977e89cd63b474104d5e327d5f13
Chris Cynarski <chris@cynarski.net>
2016-11-14 00:15:59 +0000

    Some testing for basic model added.

---
+++ tests/eloquent/BlogPost.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Class BlogPost
+ *
+ * @property string $id
+ * @property string $article_title
+ * @property string $article_intro
+ * @property string $article_body
+ */
+class BlogPost extends Illuminate\Database\Eloquent\Model
+{
+
+}

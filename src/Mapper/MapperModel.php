8d876278e0b1c153927781a0d57be637cc8732e6
Chris Cynarski <krzysztof@cynarski.net>
2019-03-25 13:36:18 +0000

    Allow MapperModel::collection()

--- src/Mapper/MapperModel.php
+++ src/Mapper/MapperModel.php
@@ -2,12 +2,33 @@
 
 namespace Fnp\Dto\Mapper;
 
+use Fnp\Dto\Collection\DtoCollectionFactory;
 use Fnp\Dto\Common\DtoMapperFill;
 use Fnp\Dto\Common\DtoToArray;
+use Fnp\Dto\Exception\DtoClassNotExistsException;
 use Fnp\Dto\Flex\DtoModel;
+use Tightenco\Collect\Support\Collection;
 
 class MapperModel extends DtoModel
 {
     use DtoToArray;
     use DtoMapperFill;
+
+    /**
+     * Make model collection with initial data
+     *
+     * @param mixed  $items Data
+     *
+     * @param string $key   For associative collection provide a model key
+     *
+     * @return Collection|null
+     */
+    public static function collection($items, $key = NULL)
+    {
+        try {
+            return DtoCollectionFactory::make(get_called_class(), $items, $key);
+        } catch (DtoClassNotExistsException $e) {
+            return new Collection([]);
+        }
+    }
 }
